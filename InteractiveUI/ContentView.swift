6650a4e2fcb4bef2608e65376c568f8ffa7f47c5
Lia <liapantusa@gmail.com>
2023-07-13 13:56:53 -0700

    Initial Commit

---
+++ InteractiveUI/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  InteractiveUI
+//
+//  Created by scholar on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var textTitle = "What is your name?"
+    
+    var body: some View {
+        
+        VStack {
+            
+            Text(textTitle)
+                .font(.title)
+            
+            TextField("Type first name here..", text: $firstName)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            
+            TextField("Type last name here..", text: $lastName)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            
+            Button("Submit Name") {
+                textTitle = "Welcome, \(firstName) \(lastName)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
